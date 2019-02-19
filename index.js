fbf4cbbabfebd073020225a1bf23a619f0fc5886
KROHNICK <business.kanekperez@gmail.com>
2019-02-19 13:05:05 -0800

    Server Get Endpoint

--- index.js
+++ index.js
@@ -16,12 +16,23 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+server.get("/", (req, res) => {
+  res.send("Server Works.");
+});
+// Server working properly on postman.
+
 // endpoints here
 
-// server.get("/", (req, res) => {
-//   res.send("Server Works.");
-// });
-// Server working properly on postman.
+server.get("/api/zoos", async (req, res) => {
+  try {
+    const zoos = await db("zoos");
+    res.status(200).json(zoos);
+  } catch (err) {
+    res.status(500).json({
+      error: "Some useful error message"
+    });
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
